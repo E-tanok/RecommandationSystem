821592eff0325beb1608d98db367a0d83fc1d860
mkonate <gkonate.malick@gmail.com>
2017-11-21 23:20:59 +0100

    Modification du fichier models.py Run soir 1

--- recommendation/models.py
+++ recommendation/models.py
@@ -11,22 +11,27 @@ from .views import app
 # Create database connection object
 #db = SQLAlchemy(app)
 
-if os.environ.get('DATABASE_URL') is None:
-    db = SQLAlchemy(app)
-    conn = sqlite3.connect('app.db')
 
-else:
-    import urllib.parse
-    url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
-    db = "dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname)
 
 def init_db():
+
+    if os.environ.get('DATABASE_URL') is None:
+        db = SQLAlchemy(app)
+        conn = sqlite3.connect('app.db')
+
+    else:
+        from urllib import parse
+        db = SQLAlchemy(app)
+        parse.uses_netloc.append("postgres")
+        url = parse.urlparse(os.environ["DATABASE_URL"])
+        conn = psycopg2.connect(database=url.path[1:],user=url.username,
+        password=url.password,host=url.hostname,port=url.port)
+        db = SQLAlchemy(app)
+        #db = "dbname=%s user=%s password=%s host=%s " % (url.path[1:], url.username, url.password, url.hostname)
+
     db.drop_all()
     db.create_all()
 
-    conn = psycopg2.connect(db)
-
-        #conn = psycopg2.connect('app.db')
 
     c = conn.cursor()
     c.executescript('drop table if exists Content;')
