55204bd7ff798329c804a8c4539c34ae003d3f44
mkonate <gkonate.malick@gmail.com>
2017-11-22 08:15:53 +0100

    Modification du code models.py apres compatibilite sur machine principale

--- recommendation/__init__.py
+++ recommendation/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 
-from .views import app #On a d�j� cr�� l'objet "app" dans le script views.py
+from .views import app
 from . import models
 
 models.db.init_app(app)
 @app.cli.command()
 def init_db():
     models.init_db()
+    
 @app.cli.command()
 def get_db():
     models.get_db()
--- recommendation/models.py
+++ recommendation/models.py
@@ -1,4 +1,3 @@
-#import enum
 from flask_sqlalchemy import SQLAlchemy
 import logging as lg
 import sqlite3
@@ -18,23 +17,18 @@ def init_db():
     db.create_all()
 
     if os.environ.get('DATABASE_URL') is None:
-        #db = SQLAlchemy(app)
+
         conn = sqlite3.connect('app.db')
         c = conn.cursor()
+        c.executescript('drop table if exists Content;')
+
         df = pd.read_csv('recommandation_system_light.csv')
         df.to_sql('Content', conn, if_exists='append', index=False)
+
         db.session.commit()
         #Content.query.all()
         lg.warning('Database initialized!')
 
     else:
         from urllib import parse
-        db = SQLAlchemy(app)
-        parse.uses_netloc.append("postgres")
-        url = parse.urlparse(os.environ["DATABASE_URL"])
-        conn = psycopg2.connect(database=url.path[1:],user=url.username,
-        password=url.password,host=url.hostname,port=url.port)
-
-        db.session.commit()
-        #Content.query.all()
-        lg.warning('Database initialized!')
+        #db = SQLAlchemy(app)
